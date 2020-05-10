05b60a9593633aacacfaf91fd8a72dece3905f9f
Olatide F <tide1988@live.com>
2020-05-10 02:06:39 -0400

    Add project files.

---
+++ GridPatch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Reflection;
+using System.Threading.Tasks;
+using NLog.Fluent;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Blocks;
+using Torch.Managers;
+using Torch.Managers.PatchManager;
+using VRage.Game.ObjectBuilders.Components;
+using VRage.Network;
+
+namespace GridSplitNameKeeper
+{
+    [PatchShim]
+    public static class GridPatch
+    {
+        
+        private static readonly MethodInfo NewNameRequest = typeof(MyCubeGrid).GetMethod("OnChangeDisplayNameRequest", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static void Patch(PatchContext ctx)
+        {
+            ctx.GetPattern(typeof(MyCubeGrid).GetMethod("MoveBlocks",  BindingFlags.Static|BindingFlags.NonPublic)).Suffixes
+                .Add(typeof(GridPatch).GetMethod(nameof(OnGridSplit), BindingFlags.Static| BindingFlags.NonPublic));
+        }
+
+        private static void OnGridSplit(ref MyCubeGrid from, ref MyCubeGrid to)
+        {
+            var newName = GetName(from.DisplayName);
+            
+            var newGrid = to;
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(100);
+                NetworkManager.RaiseEvent(newGrid, NewNameRequest, newName);
+            });
+
+        }
+
+        private static string GetName(string current)
+        {
+            double count = 0;
+            var grids = MyEntities.GetEntities().OfType<MyCubeGrid>().ToList();
+            foreach (var grid in grids)
+            {
+                if (!grid.DisplayName.Contains(current)) continue;
+                count++;
+            }
+
+            return $"{current} {count:00}";
+        }
+
+    }
+}
---
+++ GridSplitNameKeeperCore.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+using Sandbox.Engine.Multiplayer;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Blocks;
+using Torch;
+using Torch.API;
+using Torch.API.Managers;
+using Torch.API.Session;
+using Torch.Managers;
+using Torch.Session;
+using Torch.Utils;
+using VRage.Network;
+
+namespace GridSplitNameKeeper
+{
+    public class GridSplitNameKeeperCore : TorchPluginBase
+    {
+        public readonly Logger Log = LogManager.GetLogger("GridSplitNameKeeper");
+        public static GridSplitNameKeeperCore Instance { get; private set; }
+        private TorchSessionManager _sessionManager;
+
+
+        public override void Init(ITorchBase torch)
+        {
+            base.Init(torch);
+            Instance = this;
+            _sessionManager = Torch.Managers.GetManager<TorchSessionManager>();
+            if (_sessionManager != null)
+                _sessionManager.SessionStateChanged += SessionChanged;
+        }
+
+        private void SessionChanged(ITorchSession session, TorchSessionState newstate)
+        {
+            switch (newstate)
+            {
+                case TorchSessionState.Loading:
+                    break;
+                case TorchSessionState.Loaded:
+                    break;
+                case TorchSessionState.Unloading:
+                    break;
+                case TorchSessionState.Unloaded:
+                    break;
+                default:
+                    return;
+            }
+        }
+
+
+
+    }
+}
